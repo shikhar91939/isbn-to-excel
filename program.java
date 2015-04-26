42531580a4f401bb18fb5dacb9f90e7f8a0945ae
shikhar91939 <shikhar.91939@gmail.com>
2015-04-26 18:34:09 +0530

    next:check if the file is being written

--- program.java
+++ program.java
@@ -5,15 +5,28 @@ class Main
 	
 	public static void main(String args[]) throws IOException
 	{
-//		System.out.println("Welcome Back :)");
 		FileInputStream fis = new FileInputStream("outliers.xml");
 		DataInputStream dis = new DataInputStream(fis);
 		BufferedReader br = new BufferedReader(new InputStreamReader(dis));
 		
 		String line ;
+		PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter("book_details.csv", false)))
+		printWriter.println("title,contentVersion,creator,date,description,format,identifier,language,publisher,subject");
 		
 		while((line=br.readLine()) != null){
-			System.out.println(line);
+			String title="";
+			String contentVersion="";
+			String creator="";
+			String date="";
+			String description="";
+			String format="";
+			String identifier="";
+			String language="";
+			String publisher="";
+			String subject="";
+
+
+
 		}
 		
 	}
